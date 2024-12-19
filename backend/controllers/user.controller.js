cb0cdb3dd7d93eec4f97ab1b6b72253aa8e7ce34
Adriana <goiua4493@gmail.com>
2024-12-19 13:57:53 +0200

    User controllers added

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,200 @@
+import bcrypt from "bcryptjs";
+import { v2 as cloudinary } from "cloudinary";
+
+import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
+
+export const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const user = await User.findOne({ username }).select("-password");
+    if (!user) {
+      return res.status(404).json({ error: "Could not find user" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error in getUserProfile: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getSuggestedUsers = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const usersToFollow = await User.aggregate([
+      {
+        $match: {
+          followers: { $nin: [userId] },
+          _id: { $ne: userId },
+        },
+      },
+      {
+        $sample: {
+          size: 3,
+        },
+      },
+      {
+        $project: {
+          password: 0,
+        },
+      },
+    ]);
+
+    res.status(200).json(usersToFollow);
+  } catch (error) {
+    console.error("Error in getSuggestedUsers: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateUserProfile = async (req, res) => {
+  try {
+    const {
+      username,
+      fullName,
+      bio,
+      link,
+      currentPassword,
+      newPassword,
+      email,
+    } = req.body;
+    let { profileImg, coverImg } = req.body;
+    const userId = req.user._id;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: "Could not find user" });
+    }
+
+    if (
+      (!newPassword && currentPassword) ||
+      (newPassword && !currentPassword)
+    ) {
+      return res.status(400).json({
+        error: "Please provide both current and new password",
+      });
+    }
+
+    if (currentPassword && newPassword) {
+      const isMatch = await bcrypt.compare(currentPassword, user.password);
+
+      if (!isMatch)
+        return res
+          .status(400)
+          .json({ error: "The current password is not correct. Try again" });
+      if (newPassword.length < 6)
+        return res
+          .status(400)
+          .json({ error: "Password must be at least 6 characters long" });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
+    }
+
+    if (username && user.username !== username) {
+      const existingUser = await User.findOne({ username });
+      if (existingUser) {
+        return res
+          .status(400)
+          .json({ error: "Can not update user: username already taken" });
+      }
+    }
+
+    if (email && user.email !== email) {
+      const existingEmail = await User.findOne({ email });
+      if (existingEmail) {
+        return res
+          .status(400)
+          .json({ error: "Can not update user: email already taken" });
+      }
+
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        return res
+          .status(400)
+          .json({ error: "Can not update user: invalid email format" });
+      }
+    }
+
+    if (profileImg) {
+      if (user.profileImg) {
+        await cloudinary.uploader.destroy(
+          user.profileImg.split("/").pop().split(".")[0]
+        );
+      }
+      const uploadedRes = await cloudinary.uploader.upload(profileImg);
+      profileImg = uploadedRes.secure_url;
+      user.profileImg = profileImg;
+    }
+
+    if (coverImg) {
+      if (user.coverImg) {
+        await cloudinary.uploader.destroy(
+          user.coverImg.split("/").pop().split(".")[0]
+        );
+      }
+      const uploadedRes = await cloudinary.uploader.upload(coverImg);
+      coverImg = uploadedRes.secure_url;
+      user.coverImg = coverImg;
+    }
+
+    user.username = username || user.username;
+    user.fullName = fullName || user.fullName;
+    user.email = email || user.email;
+    user.bio = bio !== undefined ? bio : user.bio;
+    user.link = link !== undefined ? link : user.link;
+
+    await user.save();
+
+    user.password = null;
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error in updateUserProfile: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const followUnfollowUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    if (id === req.user._id.toString()) {
+      return res.status(400).json({ error: "You can't follow yourself" });
+    }
+
+    if (!userToModify || !currentUser) {
+      return res.status(404).json({ error: "Could not find user" });
+    }
+
+    const isFollowing = userToModify.followers.includes(req.user._id);
+
+    if (isFollowing) {
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      res.status(200).json({ message: "User unfollowed succesfully" });
+    } else {
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, {
+        $push: { following: id },
+      });
+
+      const newNotification = new Notification({
+        from: req.user._id,
+        to: id,
+        type: "follow",
+      });
+      await newNotification.save();
+
+      res.status(200).json({ message: "User followed successfully" });
+    }
+  } catch (error) {
+    console.error("Error in followUnfollowUser: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
---
+++ backend/models/notification.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import User from "./user.model.js";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    from: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    to: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    type: {
+      type: String,
+      enum: ["follow", "like"],
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model("Notification", notificationSchema);
+
+export default Notification;
